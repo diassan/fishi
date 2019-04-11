afeb2dfc8c1bb7da5b1ac94915da63ba70a5180c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-11 18:24:15 +0000

    Deploy diassan/fishi to github.com/diassan/fishi.git:gh-pages

--- js/lib.js
+++ js/lib.js
@@ -1,11 +1,11 @@
 export function bonus(items) {
-    let SumBonus = 0;
+    let sumBonus = 0;
     console.log(typeof(items));
     for (const item of items) {
        const limit = 10000;
        if (item > limit) {
-       SumBonus += (item - limit) * 0.05;
+       sumBonus += (item - limit) * 0.05;
        }
     }
-    return SumBonus;
+    return sumBonus;
 }
