12ec4315076f4a3cb13220506296d822d6f35268
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-11 18:25:11 +0000

    Deploy diassan/fishi to github.com/diassan/fishi.git:gh-pages

--- js/test/lib.test.js
+++ js/test/lib.test.js
@@ -1,9 +1,9 @@
 import {bonus} from "../lib.js";
 
-test('should bonus', ()=>{
-const items = [8000, 10000,13000,20000];
-const expected = 650;
+test('should bonus', () => {
+    const items = [8000, 10000, 13000, 20000];
+    const expected = 650;
 
-const result = bonus(items);
-expect(result).toBe(expected);
+    const result = bonus(items);
+    expect(result).toBe(expected);
 });
